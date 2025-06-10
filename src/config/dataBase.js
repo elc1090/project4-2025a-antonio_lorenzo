fedd820b200a3968bfaac722867123265f890f16
Antônio Amadeu Dall'Agnol Rohr <aarohr@inf.ufsm.br>
2025-06-10 16:02:16 -0300

    fix: arrumando deploy na render

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     // Nome do banco
-  process.env.DB_USER,     // Usuário
-  process.env.DB_PASSWORD, // Senha
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: false, // ou console.log para ver as queries
-  }
-);
 
-module.exports = sequelize;
+
+module.exports = {
+  dialect: 'postgres',
+  url: process.env.DATABASE_URL,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+};
