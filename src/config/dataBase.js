71b8c03233bb72cd75a57ea261ad362e439449cb
Antônio Amadeu Dall'Agnol Rohr <aarohr@inf.ufsm.br>
2025-06-10 16:12:22 -0300

    fix: duplicata removida

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -2,9 +2,6 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 
-
-const { Sequelize } = require('sequelize');
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
