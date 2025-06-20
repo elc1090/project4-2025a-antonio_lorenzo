2a78e77ba9fe7a23cdf722f59189d9dc0e1f0b36
Lorenzo Taschetto <lorenzotaschetto04@gmail.com>
2025-06-19 22:08:48 -0300

    fix: index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,19 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/AuthContext'; // Importe o AuthProvider
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Envolva o App com o AuthProvider */}
-    <AuthProvider>
-      <App />
+    {}
+    <AuthProvider>      {/* Provedor de Autenticação */}
+      <ThemeProvider>   {}
+        <App />
+      </ThemeProvider>
     </AuthProvider>
   </React.StrictMode>
 );
