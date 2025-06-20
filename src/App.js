843aee402405b2bbf8fb368a732d8f331e07d835
Antônio Amadeu Dall'Agnol Rohr <103676565+AntonioDallAgnol@users.noreply.github.com>
2025-06-19 23:41:11 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowseRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 
